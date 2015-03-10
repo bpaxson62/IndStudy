82726e4d4ca916fca593643df559ca5f918fbbc1
unknown <bpaxson62@gmail.com>
2015-03-09 22:24:29 -0400

    Renaming of folders

--- ind_study/football_crawler/settings.py
+++ ind_study/football_crawler/settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Scrapy settings for tutorial project
+# Scrapy settings for ind_study project
 #
 # For simplicity, this file contains only the most important settings by
 # default. All the other settings are documented here:
@@ -8,10 +8,10 @@
 #     http://doc.scrapy.org/en/latest/topics/settings.html
 #
 
-BOT_NAME = 'tutorial'
+BOT_NAME = 'ind_study'
 
-SPIDER_MODULES = ['tutorial.spiders']
-NEWSPIDER_MODULE = 'tutorial.spiders'
+SPIDER_MODULES = ['ind_study.spiders']
+NEWSPIDER_MODULE = 'ind_study.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'tutorial (+http://www.yourdomain.com)'
+#USER_AGENT = 'ind_study (+http://www.yourdomain.com)'
