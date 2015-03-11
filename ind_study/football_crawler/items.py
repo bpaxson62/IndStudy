cce603100838627d0ab09b58a843ad21acb1f249
unknown <bpaxson62@gmail.com>
2015-03-10 22:44:30 -0400

    Finished depth spider version 1 added csv ignore

--- ind_study/football_crawler/items.py
+++ ind_study/football_crawler/items.py
@@ -4,9 +4,14 @@
 #
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
+from datetime import datetime
 
 import scrapy
+from scrapy.contrib.loader import ItemLoader
+from scrapy.contrib.loader.processor import MapCompose, TakeFirst
 
+def parse_date(self, date):
+    return datetime.strptime(date[0][8:], "%a %b %d, %Y %H:%M %p").ctime()
 
 class PostItem(scrapy.Item):
     # define the fields for your item here like:
@@ -23,8 +28,19 @@ class AuthorItem(scrapy.Item):
     geo_location = scrapy.Field()
     num_posts = scrapy.Field()
 
-class TestItem(scrapy.Item):
+class DepthItem(scrapy.Item):
     post_title = scrapy.Field()
-    page = scrapy.Field()
+    page = scrapy.Field(default=1)
     team = scrapy.Field()
     date = scrapy.Field()
+
+class DepthItemLoader(ItemLoader):
+    default_input_processor = MapCompose(unicode.strip)
+    default_output_processor = TakeFirst()
+
+    team_in = TakeFirst()
+    team_out = MapCompose(unicode.strip)
+
+    # date_in = TakeFirst()
+    date_out = parse_date
+
